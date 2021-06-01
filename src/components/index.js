2a31bd8b185cbbae921e7c9ad792115c3e46137f
jakemarotta <marottajb@gmail.com>
2021-06-01 11:43:46 -0400

    Removes old component export

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as HoursOfOperation } from "./HoursOfOperation.vue";
-export { default as LoadingOverlay } from "./LoadingOverlay.vue";
 export { default as MenuItem } from "./MenuItem.vue";
 export { default as MenuSection } from "./MenuSection.vue";
